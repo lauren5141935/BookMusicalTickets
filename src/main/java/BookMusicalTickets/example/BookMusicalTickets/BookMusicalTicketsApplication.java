5501a40e0a8e2fd9d585883babd0705a84e6ce84
Lauren Jung <lauren5141935@gmail.com>
2024-08-31 03:24:29 +0900

    Initial commit

---
+++ src/main/java/BookMusicalTickets/example/BookMusicalTickets/BookMusicalTicketsApplication.java
@@ -0,0 +1,13 @@
+package BookMusicalTickets.example.BookMusicalTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMusicalTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMusicalTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BookMusicalTickets/example/BookMusicalTickets/BookMusicalTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package BookMusicalTickets.example.BookMusicalTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMusicalTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
